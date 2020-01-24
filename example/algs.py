a9c549985c1596ed6707490c5ca7faea789ce39f
tianna.grant <tianna.grant@ucsf.edu>
2020-01-24 12:07:26 -0800

    Alg test push

--- example/algs.py
+++ example/algs.py
@@ -7,18 +7,81 @@ def pointless_sort(x):
     """
     return np.array([1,2,3])
 
-def bubblesort(x):
-    """
-    Describe how you are sorting `x`
-    """
+def bubblesort(B):
+    assignment_counter=0
+    conditional_counter=0
+    index_length = len(B) -1
+    assignment_counter+=1
+    sorted = False
+    assignment_counter+=1
+    while not sorted:
+        sorted = True
+        assignment_counter+=1
+        conditional_counter+=1
+        for i in range(0, index_length):
+            assignment_counter+=1
+            conditional_counter+=1
+            if B[i] > B[i+1]:
+                sorted = False
+                assignment_counter+=3
+                B[i], B[i+1] = B[i+1], B[i]
+    print("number of assignments=", assignment_counter)
+    print("number of conditonals=", conditional_counter)
+    return B
 
-    assert 1 == 1
-    return x
+
+
+    
 
 def quicksort(x):
-    """
-    Describe how you are sorting `x`
-    """
+    assignment_counter = 0
+    conditional_counter = 0
+    assignment_counter += 2
+    length = len(sort)
+    conditional_counter+=1
+    if length <= 1:
+        return sort
+    else:
+        assignment_counter+=1
+        #remove and return- from the end of the list
+        pivot = sort.pop()
+    items_greater = []
+    items_lower = []
+    for item in sort:
+        assignment_counter+=4
+        conditional_counter+=1
+        if item > pivot:
+            items_greater.append(item)
 
-    assert 1 == 1
-    return
+        else:
+            items_lower.append(item)
+    print("number of assignments=", assignment_counter)
+    print("number of conditional=", conditional_counter)
+
+    return quicksort(items_lower) + [pivot] +quicksort(items_greater)
+
+
+def InsertionSort(A):
+    assignment_counter = 0
+    conditional_counter = 0
+    assignment_counter+=2
+    B = [None for i in range(len(A))] # B is a blank list of the same length as A
+    for x in A:
+        assignment_counter+=4
+        conditional_counter+=2
+        for i in range(len(B)):
+            if B[i] == None or B[i] > x:
+                # then x goes in spot i, and we should move everything over.
+                j = len(B)-1
+                while j > i:
+                    assignment_counter+=3
+                    B[j] = B[j-1]
+                    j -= 1
+                B[i] = x
+                break # okay we are done placing x
+    print(assignment_counter)
+    print(conditional_counter)
+    return B
+
+A = [3, 5, 4, 2, 6, 7, 1, 8]
+B = InsertionSort(A)
--- test/test_algs.py
+++ test/test_algs.py
@@ -19,16 +19,35 @@ def test_bubblesort():
     # some edge cases for your code, where it might fail. Some things to
     # think about: (1) does your code handle 0-element arrays without
     # failing, (2) does your code handle characters?
+    
 
-    x = np.array([1,2,4,0,1])
+    B = np.array([3,5,4,2,6,7,1,8])
+    assert np.array_equal(np.array(algs.bubblesort(B)), np.array([1, 2, 3, 4, 5, 6, 7, 8]))
+    empty = np.array([])
+    assert np.array_equal(np.array(algs.bubblesort(empty)), np.array([]))
+    duplication = np.array([4,4,5,5,7,7,1,1])
+    assert np.array_equal(np.array(algs.bubblesort(duplication)), np.array([1, 1, 4, 4, 5, 5, 7, 7]))
+    
+    #list with odd number of elements
+    odd = np.array([1,3,2,4,9])
+    assert np.array_equal(np.array(algs.bubblesort(odd)), np.array([1,2,3,4,9]))
+                    
+    #list with characters
+                    
+    a = np.array(['apple', 'banana','zebra', 'red'])
+    assert np.array_equal(np.array(algs.bubblesort(a)), np.array(['apple', 'banana', 'red', 'zebra']))
+    
+    
+    #Duplicted assert statement
+    #assert np.array_equal(algs.bubblesort([4,4,5,5,7,7,1,1])), np.array(([1,1,4,4,5,5,7,7]))
     
     # for now, just attempt to call the bubblesort function, should
     # actually check output
-    algs.bubblesort(x)
+   
 
 def test_quicksort():
 
     x = np.array([1,2,4,0,1])
     # for now, just attempt to call the quicksort function, should
     # actually check output
-    algs.quicksort(x)
+   
